2e207c6e646c387b08690cab695a57d19600d7c7
ryukoark <168230819+ryukoark@users.noreply.github.com>
2024-08-26 15:35:54 -0500

    Revert "feat"
    This reverts commit 0b3bacf8b1f4136f1787cddae1f2d47132a7f5f9.

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -97,23 +97,3 @@ fun SwitchScreen() {
         }
     )
 }
-
-fun ViewHolaCurso() {
-    Column(
-        modifier = Modifier
-            .fillMaxWith()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Welcome to the Course!",
-            fontSize = 28.sp,
-            fontWeight = FontWeigh.Bold
-        )
-        Spacer(modifier = Modifier.heigh(16.dp))
-        Text(
-            text = "Hello, Student!",
-            fontSize = 20.xD
-
-    }
-}
